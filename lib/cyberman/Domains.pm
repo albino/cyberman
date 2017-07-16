e3d00d0683023aeeaad4be301052233bb3c09efd
Al Beano <albino@autistici.org>
2017-07-16 18:06:51 +0100

    Domain registration (excluding communication with nsd)
    Domains are registered in the sqlite database but the information is not sent to nsd; nothing will really happen.

--- lib/cyberman/Domains.pm
+++ lib/cyberman/Domains.pm
@@ -20,4 +20,55 @@ get '/domains' => sub {
   }
 };
 
+post '/domains/new' => sub {
+  my %errs;
+
+  if (!param("name")) {
+    $errs{"e_no_name"} = 1;
+  }
+
+  my $name = lc param("name");
+
+  if (scalar(keys(%errs)) == 0) {
+    if (param("name") !~ m/^[a-z0-9]([a-z0-9\-_]*[a-z0-9])?$/) {
+      $errs{"e_chars"} = 1;
+    }
+  }
+
+  if (scalar(keys(%errs)) == 0) {
+    my $result = database->quick_select(
+      "domain",
+      {
+        "name" => param("name"),
+      },
+    );
+
+    if ($result) {
+      $errs{"e_exists"} = 1;
+    }
+  }
+
+  if (scalar(keys(%errs)) != 0) {
+    return template 'domains/new' => {
+      params,
+      %errs,
+      error => 1,
+    };
+  }
+
+  # TODO: send domains to nsd
+
+  database->quick_insert(
+    "domain",
+    {
+      "name" => $name,
+      "ownerid" => vars->{"auth"},
+    },
+  );
+
+  template 'redir' => {
+    "redir" => "../domains",
+  };
+};
+
 true;
