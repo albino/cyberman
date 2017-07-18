cdab16e9db8553ee97b12516ffdc9f0feba60ac5
Al Beano <albino@autistici.org>
2017-07-18 13:40:48 +0100

    Add account control panel

--- lib/cyberman/Account.pm
+++ lib/cyberman/Account.pm
@@ -2,4 +2,114 @@ package cyberman::Account;
 use Dancer2 appname => "cyberman";
 use Dancer2::Plugin::Database;
 
+use cyberman::Helper;
+
+get '/account' => sub {
+  return auth_test() if auth_test();
+
+  my $user = database->quick_select(
+    "user",
+    {
+      "id" => vars->{"auth"},
+    },
+  );
+
+  template 'account' => {
+    "user" => $user,
+  };
+};
+
+post '/account' => sub {
+  return auth_test() if auth_test();
+
+  my %errs;
+  my $new_pass = 0;
+
+  my $user = database->quick_select (
+    "user",
+    {
+      "id" => vars->{"auth"},
+    }
+  );
+
+  if (!param("email")) {
+    $errs{"e_no_email"} = 1;
+  }
+
+  if (param("password") || param("npassword") || param("npassword2")) {
+    $new_pass = 1;
+
+    my ($o_hash, $o_salt) = hash_password(param("password"), $user->{"salt"});
+    if ($o_hash ne $user->{"password"}) {
+      $errs{"e_wrong_pass"} = 1;
+    }
+
+    if (param "npassword" ne param "npassword2") {
+      $errs{"e_pass_mismatch"} = 1;
+    } elsif (length(param "npassword") < 8) {
+      $errs{"e_pass_len"} = 1;
+    }
+  }
+
+  if (scalar(keys %errs) != 0) {
+    return template 'account' => {
+      "user" => $user,
+      error => 1,
+      %errs,
+    };
+  }
+
+  if (param("email") ne $user->{"email"}) {
+
+    # TODO: verify email address here
+
+    database->quick_update (
+      "user",
+      {
+        "id" => vars->{"auth"},
+      },
+      {
+        "email" => param "email",
+      },
+    );
+  }
+
+  if ($new_pass) {
+    my ($hash, $salt) = hash_password(param "npassword");
+    database->quick_update (
+      "user",
+      {
+        "id" => vars->{"auth"},
+      },
+      {
+        "password" => $hash,
+        "salt" => $salt,
+      },
+    );
+
+    database->quick_delete (
+      "session",
+      {
+        "uid" => vars->{"auth"},
+      },
+    );
+
+    return template 'redir' => {
+      "redir" => "login?pwchange=1",
+    };
+  }
+
+  $user = database->quick_select (
+    "user",
+    {
+      "id" => vars->{"auth"},
+    },
+  );
+
+  template 'account' => {
+    updated => 1,
+    user => $user,
+  };
+};
+
 true;
